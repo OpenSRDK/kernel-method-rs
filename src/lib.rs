a70fec418bb051685677be8aab77fc21ff2bdeb8
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-01 19:39:40 +0900

    chore: remove partial eq

--- src/lib.rs
+++ src/lib.rs
@@ -24,50 +24,50 @@ pub mod mul;
 pub mod periodic;
 pub mod rbf;
 
-pub trait Value: Clone + Debug + PartialEq + Send + Sync {}
-impl<T> Value for T where T: Clone + Debug + PartialEq + Send + Sync {}
+pub trait Value: Clone + Debug + Send + Sync {}
+impl<T> Value for T where T: Clone + Debug + Send + Sync {}
 
 pub trait Kernel<T>: Clone + Debug + Send + Sync
 where
-  T: Value,
+    T: Value,
 {
-  fn params_len(&self) -> usize;
+    fn params_len(&self) -> usize;
 
-  fn value(&self, params: &[f64], x: &T, xprime: &T) -> Result<f64, KernelError>;
+    fn value(&self, params: &[f64], x: &T, xprime: &T) -> Result<f64, KernelError>;
 
-  fn value_with_grad(
-    &self,
-    params: &[f64],
-    x: &T,
-    xprime: &T,
-  ) -> Result<(f64, Vec<f64>), KernelError>;
+    fn value_with_grad(
+        &self,
+        params: &[f64],
+        x: &T,
+        xprime: &T,
+    ) -> Result<(f64, Vec<f64>), KernelError>;
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum KernelError {
-  #[error("parameters length mismatch")]
-  ParametersLengthMismatch,
-  #[error("invalid parameter")]
-  InvalidParameter,
-  #[error("invalid argument")]
-  InvalidArgument,
+    #[error("parameters length mismatch")]
+    ParametersLengthMismatch,
+    #[error("invalid parameter")]
+    InvalidParameter,
+    #[error("invalid argument")]
+    InvalidArgument,
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::*;
-  #[test]
-  fn it_works() {
-    let kernel = RBF + Bias * Linear + Bias * Periodic + Bias * ARD(3);
-    let (func, grad) = kernel
-      .value_with_grad(
-        &vec![1.0; kernel.params_len()],
-        &vec![1.0, 2.0, 3.0],
-        &vec![10.0, 20.0, 30.0],
-      )
-      .unwrap();
+    use crate::*;
+    #[test]
+    fn it_works() {
+        let kernel = RBF + Bias * Linear + Bias * Periodic + Bias * ARD(3);
+        let (func, grad) = kernel
+            .value_with_grad(
+                &vec![1.0; kernel.params_len()],
+                &vec![1.0, 2.0, 3.0],
+                &vec![10.0, 20.0, 30.0],
+            )
+            .unwrap();
 
-    println!("{}", func);
-    println!("{:#?}", grad);
-  }
+        println!("{}", func);
+        println!("{:#?}", grad);
+    }
 }
