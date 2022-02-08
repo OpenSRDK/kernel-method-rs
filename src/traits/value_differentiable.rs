345ab72bfab355cd2fd7424dd14853c56c247134
Rubens <91782701+tibetsou@users.noreply.github.com>
2022-02-08 22:04:27 +0900

    feat: addに微分を実装

--- src/traits/value_differentiable.rs
+++ src/traits/value_differentiable.rs
@@ -4,10 +4,5 @@ pub trait ValueDifferentiableKernel<T>: PositiveDefiniteKernel<T>
 where
     T: Value,
 {
-    fn ln_diff_value(
-        &self,
-        params: &[f64],
-        x: &Vec<f64>,
-        xprime: &Vec<f64>,
-    ) -> Result<Vec<f64>, KernelError>;
+    fn ln_diff_value(&self, params: &[f64], x: &T, xprime: &T) -> Result<Vec<f64>, KernelError>;
 }
